1c45ee5986bc12ea108ff3c8096f2e21d21c17ae
Luka Popijac <luka.popijac@gmail.com>
2025-03-17 22:05:48 -0700

    setup for both dev and build for deployment to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import {defineConfig} from 'astro/config';
 
 export default defineConfig({
 	outDir: './docs',
+	base: process.env.NODE_ENV === 'production' ? '/cube-instructions/' : '/',
 	build: {
 		assets: 'assets',
 	}
